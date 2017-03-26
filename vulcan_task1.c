d9bae832b73e5cf9d7b95722a445dc4e72c7f8ce
christopher bowen <christopherbowen@mail.weber.edu>
2017-03-26 02:59:20 +0000

    initial commit

---
+++ vulcan_task1.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vulcan_task1.c
+ *    Description:  takes two strings and checks sees if they have similarities and then returns another string
+ *
+ *        Created:  03/26/2017 01:47:22 AM
+ *         Author:  Christopher Bowen (), christopherbowen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+void Usage(char *argv[]);
+/* Main Program */
+int main(int argc, char *argv[])
+{
+	char arg1
+	if(argv[1] == "--help")
+	{
+		Usage(*argv)
+	}
+	return 0;
+}
+
+
+/* Function Defenitions */
+void Usage(char *argv[])
+{
+
+	{
+		printf("Help information\n");
+		printf("Usage ./task1 <str1> <str2>\n");
+		printf("Program checks if str2 is part of str1\n");
+	}
+	return;
+}
+
+
