77567c6d7d1e672988c7f7b8c14246994eb6297a
christopher bowen <christopherbowen@mail.weber.edu>
2017-03-28 04:35:07 +0000

    defined strings

--- vulcan_task1.c
+++ vulcan_task1.c
@@ -10,16 +10,31 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include <string.h>
+
+#define SIZE 81
 /* Function Prototypes */
 void Usage(char *argv[]);
+char StringIn(char *arg1[], char *arg2[]);
 /* Main Program */
 int main(int argc, char *argv[])
 {
-	char arg1
-	if(argv[1] == "--help")
+	//first check and see if they insert the right amount of parameters
+	//if not call Usage
+	if(strcmp(argv[1], "--help")==0)
 	{
-		Usage(*argv)
+		Usage(argv);
 	}
+	if(argc != 3)
+	{
+		Usage(argv);
+	}
+	//define two strings which are my two strings i will use later in StringIn
+	char first[SIZE];
+	strcpy(first, argv[1]);
+
+	char second[SIZE];
+	strcpy(second, argv[2]);
 	return 0;
 }
 
@@ -27,13 +42,23 @@ int main(int argc, char *argv[])
 /* Function Defenitions */
 void Usage(char *argv[])
 {
-
+	if(strcmp(argv[1], "--help")==0)
 	{
 		printf("Help information\n");
 		printf("Usage ./task1 <str1> <str2>\n");
 		printf("Program checks if str2 is part of str1\n");
 	}
+	else
+	{
+		printf("Missing required parameters\n");
+		printf("./task1 <str1><str2>\n");
+		printf("Program checks if str2 is part of str1\n");
+	}
 	return;
 }
 
+//char StringIn(char *arg1[], char *arg2[])
+//{
+//	
+//}
 
